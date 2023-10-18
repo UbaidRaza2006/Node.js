c45d89e0ab2de4bc1e2820fd43f9ffdecd374fe7
ubaidraza2006 <ubaidmemon2006@gmail.com>
2023-10-18 19:33:37 +0500

    Delete Route

--- routes/user.js
+++ routes/user.js
@@ -41,18 +41,52 @@ router.get('/', (req,res) => {
     )
 })
 
-router.post('/',(req,res) => {
-    res.send( 'Post Route Called')
+
+router.delete('/:id',(req,res) => {
+    users.splice(req.params.id-1,req.params.id-1)
+    res.status(200).send({
+        status:200,
+        users:users
+    })
 })
 
+// Error To Be Solved
 
-router.get('/:id', (req,res) => {
-    const user=users.find((data)=>data.id)
-    res.send({msg:'Get User with ID ' + user.name + user.id + 'The Query is ' + req.query.type})
-// console.log(req.query);
+// router.post('/',(req,res) => {
+//     // res.send( 'Post Route Called')
+//     // console.log(req.body);
+//     users.push({name:req.body.name,course:req.body.course,id:users.length+1})
+//     res.status(200).send({
+//         status:200,
+//         user:users[users.length-1]
+//     })
+// })
+
+
+// Error To Be Solved
+
+
+// router.get('/:id', (req,res) => {
+//     const user=users.find((data)=>data.id)
+//     res.send({msg:'Get User with ID ' + user.name + user.id + 'The Query is ' + req.query.type})
+// // console.log(req.query);
+// })
+
+router.get('/:id',(req,res) => {
+    if(users[req.params.id-1]){
+        res.status(200).send({
+            status:200,
+            userById: users[req.params.id-1],
+            error:true
+        })
+    }
+    else{
+        res.status(500).send({
+            status:500,
+            msg:'User Not Found'
+        })
+    }
 })
-
-
 
 
 
